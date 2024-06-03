fbe6310046dd15d1b579c6dc148230281e585209
Andoromain <andoajpr@gmail.com>
2024-06-03 11:01:35 +0300

    update

--- src/pages/Pokemon/Pokemon.vue
+++ src/pages/Pokemon/Pokemon.vue
@@ -1,8 +1,13 @@
 <script setup>
-import { computed, onBeforeMount, ref } from 'vue';
-import { useStore } from 'vuex';
-import List from "./List/List.vue"
+import { computed, onBeforeMount, ref } from "vue";
+import { useStore } from "vuex";
+import List from "./List/List.vue";
 import InputText from "primevue/inputtext";
+import { useRouter } from "vue-router";
+import Button from "primevue/button";
+import ProgressSpinner from "primevue/progressspinner";
+
+const router = useRouter();
 
 const store = useStore();
 
@@ -10,36 +15,68 @@ const search = ref("");
 
 const pokemons = computed(() => store.getters.getPokemons);
 
+const loading = computed(() => store.getters.getLoading);
+
 const fetchPokemons = () => {
-    if (pokemons.value.length === 0) {
-        store.dispatch('fetchPokemons');
-    }
-}
+  if (pokemons.value.length === 0) {
+    store.dispatch("fetchPokemons");
+  }
+};
+
+const reload = () => {
+  router.go(0);
+};
+
+const deleteAll = () => {
+  store.commit("deleteAllPokemons");
+};
 
 const removePokemon = (id) => {
-    store.commit('removePokemon', id)
-}
+  store.commit("removePokemon", id);
+};
 
 onBeforeMount(() => {
-    fetchPokemons();
+  fetchPokemons();
 });
 
 const filters = computed(() => {
-    return pokemons.value.filter((item) => {
-        const nameMatch = item?.name.toLowerCase().includes(search.value.toLowerCase() ?? '') || item.apiTypes.some((type) => {
-            return type.name?.toLowerCase().includes(search.value.toLowerCase())
-        });
+  return pokemons.value.filter((item) => {
+    const nameMatch =
+      item?.name.toLowerCase().includes(search.value.toLowerCase() ?? "") ||
+      item.apiTypes.some((type) => {
+        return type.name?.toLowerCase().includes(search.value.toLowerCase());
+      });
 
-        return nameMatch;
-    });
+    return nameMatch;
+  });
 });
-
 </script>
 <template>
-    <div>
-        <div class="flex align-items-center gap-2 justify-content-center my-2">
-            <InputText placeholder="Search" type="text" v-model="search" class="w-8rem sm:w-auto" />
-        </div>
-        <List :data="search.length > 0 ? filters : pokemons" @remove="removePokemon"></List>
+  <div>
+    <div class="flex align-items-center gap-2 justify-content-center my-2">
+      <Button label="Supprimer tous" severity="danger" @click="deleteAll" />
+      <Button label="Reload" @click="reload" />
     </div>
+    <div class="flex align-items-center gap-2 justify-content-center my-2">
+      <InputText
+        placeholder="Search"
+        type="text"
+        v-model="search"
+        class="w-8rem sm:w-auto"
+      />
+    </div>
+    <template v-if="loading">
+      <div
+        class="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center"
+      >
+        <ProgressSpinner style="width: 40px; height: 60px" strokeWidth="10" />
+      </div>
+    </template>
+    <template v-else>
+      <List
+        :data="search.length > 0 ? filters : pokemons"
+        @remove="removePokemon"
+      ></List>
+    </template>
+  </div>
 </template>
--- src/store/pokemon.js
+++ src/store/pokemon.js
@@ -4,9 +4,11 @@ import { Get } from "@/Http"
 export const pokemon = {
     state: {
         pokemons: [],
+        loading: false,
     },
     getters: {
        getPokemons: (state) => state.pokemons,
+       getLoading: (state) => state.loading,
     },
     mutations: {
         setPokemons(state, pokemons) {
@@ -14,10 +16,18 @@ export const pokemon = {
         },
         removePokemon(state,id){
             state.pokemons = state.pokemons.filter((item) => item.id !== id);
-        }
+        },
+
+        deleteAllPokemons(state){
+            state.pokemons = [];
+        },
+        setLoading(state, loading) {
+            state.loading = loading;
+        },
     },
     actions: {
         async fetchPokemons({ commit }) {
+            commit("setLoading", true);
             try{
                 const {data} = await Get({url:`${API.base_url_pokemon}${API.pokemons}`});
                 console.log(data)
@@ -27,6 +37,7 @@ export const pokemon = {
             }catch(e){
                 console.error(e)
             }
+            commit("setLoading", false);
         }
     },
 }
